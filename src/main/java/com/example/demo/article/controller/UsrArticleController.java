968c0b6452d0527ccccc3ce23f3165ac1a3b60f7
hmk813 <moonkyus@hanmail.net>
2023-01-06 00:45:18 +0900

    start

---
+++ src/main/java/com/example/demo/article/controller/UsrArticleController.java
@@ -0,0 +1,79 @@
+package com.example.demo.article.controller;
+
+import com.example.demo.article.service.ArticleService;
+import com.example.demo.article.vo.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UsrArticleController {
+
+  @Autowired
+  private ArticleService articleService;
+
+  //액션 메서드 시작
+  @RequestMapping("/usr/article/doAdd")
+  @ResponseBody
+  public Article doAdd(String title, String body) {
+    Article article = articleService.writeArticle(title, body);
+
+    return article;
+  }
+
+  @RequestMapping("/usr/article/getArticles")
+  @ResponseBody
+  public List<Article> getArticles() {
+
+    return articleService.getArticles();
+  }
+
+  @RequestMapping("/usr/article/getArticle")
+  @ResponseBody
+  public Object getArticleAction(int id) {
+    Article article = articleService.getArticle(id);
+
+    if( article == null ){
+      return id + "번 게시물이 존재하지 않습니다.";
+    }
+
+    return article;
+  }
+
+  @RequestMapping("/usr/article/doDelete")
+  @ResponseBody
+  public String doDelete(int id) {
+    Article article = articleService.getArticle(id);
+
+    if( article == null ) {
+      return id + "번 게시물이 존재하지 않습니다용!";
+    }
+
+    articleService.deleteArticle(id);
+    
+    return id + "번 게시물을 삭제했다용!";
+  }
+
+
+  @RequestMapping("/usr/article/doModify")
+  @ResponseBody
+  public String doModify(int id,String title, String body) {
+    Article article = articleService.getArticle(id);
+
+    if( article == null ) {
+      return id + "번 게시물이 존재하지 않습니다용!";
+    }
+
+    articleService.modifyArticle(id, title, body);
+
+    return id + "번 게시물을 수정했다용!";
+  }
+
+
+  //액션 메서드 끝
+
+}
