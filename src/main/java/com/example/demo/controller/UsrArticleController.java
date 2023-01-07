ca0a7ec8354d812ee2c5112ab3d466c76a264432
hmk813 <moonkyus@hanmail.net>
2023-01-07 22:46:33 +0900

    수정,삭제에 ResultData 적용

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -41,7 +41,7 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/getArticles")
   @ResponseBody
-  public ResultData getArticles() {
+  public ResultData<List<Article>> getArticles() {
     List<Article> articles = articleService.getArticles();
 
     return ResultData.from("S-1","게시물 리스트입니다.", articles);
@@ -49,7 +49,7 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/getArticle")
   @ResponseBody
-  public ResultData getArticle(int id) {
+  public ResultData<Article> getArticle(int id) {
     Article article = articleService.getArticle(id);
 
     if( article == null ){
@@ -62,31 +62,31 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/doDelete")
   @ResponseBody
-  public String doDelete(int id) {
+  public ResultData<Integer> doDelete(int id) {
     Article article = articleService.getArticle(id);
 
     if( article == null ) {
-      return id + "번 게시물이 존재하지 않습니다용!";
+      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id)); //실패
     }
 
     articleService.deleteArticle(id);
-    
-    return id + "번 게시물을 삭제했다용!";
+
+    return ResultData.from("S-1",Ut.f("%d번 게시물을 삭제하였습니다.", id), id);//성공
   }
 
 
   @RequestMapping("/usr/article/doModify")
   @ResponseBody
-  public String doModify(int id,String title, String body) {
+  public ResultData doModify(int id,String title, String body) {
     Article article = articleService.getArticle(id);
 
     if( article == null ) {
-      return id + "번 게시물이 존재하지 않습니다용!";
+      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
     }
 
     articleService.modifyArticle(id, title, body);
 
-    return id + "번 게시물을 수정했다용!";
+    return ResultData.from("S-1",Ut.f("%d번 게시물을 수정하였습니다.", id), id);
   }
 
 
