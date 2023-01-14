5fb680024449606bc2f183fc4223b48fad237c9f
hmk813 <moonkyus@hanmail.net>
2023-01-14 17:41:56 +0900

    게시물 페이징을 위한 대량 게시물 생성

--- db/schema.sql
+++ db/schema.sql
@@ -1,102 +1,103 @@
-#DB생성
-DROP DATABASE IF EXISTS Board;
-CREATE DATABASE Board;
-USE Board;
+# DB 생성
+DROP DATABASE IF EXISTS sb_app_2022_t;
+CREATE DATABASE sb_app_2022_t;
+USE sb_app_2022_t;
 
-#게시물 테이블 생성
-CREATE TABLE article(
-id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-regDate DATETIME NOT NULL,
-updateDate DATETIME NOT NULL,
-title CHAR(100) NOT NULL,
-BODY TEXT NOT NULL
+# 게시물 테이블 생성
+CREATE TABLE article (
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	title CHAR(100) NOT NULL,
+	`body` TEXT NOT NULL
 );
 
-#게시물, 테스트 데이터 생성
+# 게시물, 테스트 데이터 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목1',
-BODY = '내용1';
+`body` = '내용1';
 
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목2',
-BODY = '내용2';
+`body` = '내용2';
 
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목3',
-BODY = '내용3';
+`body` = '내용3';
 
-#회원 테이블 생성
-CREATE TABLE MEMBER(
-id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-regDate DATETIME NOT NULL,
-updateDate DATETIME NOT NULL,
-loginId CHAR(20) NOT NULL,
-loginPw CHAR(60) NOT NULL,
-authLevel SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨 (3=일반, 7=관리자)',
-NAME CHAR(20) NOT NULL,
-nickname CHAR(20) NOT NULL,
-cellphoneNo CHAR(20) NOT NULL,
-email CHAR(50) NOT NULL,
-delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
-delDate DATETIME COMMENT '탈퇴날짜'
+# 회원 테이블 생성
+CREATE TABLE `member` (
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	loginId CHAR(20) NOT NULL,
+	loginPW CHAR(60) NOT NULL,
+	`authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨 (3=일반, 7=관리자)',
+	`name` CHAR(20) NOT NULL,
+	`nickname` CHAR(20) NOT NULL,
+	cellphoneNo CHAR(20) NOT NULL,
+	email CHAR(50) NOT NULL,
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
+	delDate DATETIME COMMENT '탈퇴날짜'
 );
 
-#회원, 테스트 데이터 생성(관리자 회원)
-INSERT INTO MEMBER
+# 회원, 테스트 데이터 생성(관리자 회원)
+INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
 loginId = 'admin',
 loginPw = 'admin',
 authLevel = 7,
-NAME = '관리자',
-nickname = '관리자',
+`name` = '관리자',
+`nickname` = '관리자',
 cellphoneNo = '01011111111',
-email = 'moonkyus@hanmail.net';
+email = 'gotkdqja@gmail.com';
 
-#회원, 테스트 데이터 생성(일반 회원)
-INSERT INTO MEMBER
+# 회원, 테스트 데이터 생성(일반 회원)
+INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
 loginId = 'user1',
 loginPw = 'user1',
-NAME = 'user1',
-nickname = 'user1',
-cellphoneNo = '01022222222',
-email = 'moonkyus@hanmail.net';
+`name` = 'user1',
+`nickname` = 'user1',
+cellphoneNo = '01011111111',
+email = 'gotkdqja@gmail.com';
 
-INSERT INTO MEMBER
+INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
 loginId = 'user2',
 loginPw = 'user2',
-NAME = 'user2',
-nickname = 'user2',
-cellphoneNo = '01033334444',
-email = 'moonkyus@hanmail.net';
+`name` = 'user2',
+`nickname` = 'user2',
+cellphoneNo = '01011111111',
+email = 'gotkdqja@gmail.com';
 
-#게시물 테이블에 회원정보 추가
-ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+# 게시물 테이블에 회원정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
 
-#기존 게시물에 작성자를 2번으로 지정
+# 기존 게시물의 작성자를 2번으로 지정
 UPDATE article
 SET memberId = 2
 WHERE memberId = 0;
 
-#게시판 테이블 생성
+
+# 게시판 테이블 생성
 CREATE TABLE board (
-id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-regDate DATETIME NOT NULL,
-updateDate DATETIME NOT NULL,
-`code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice=공지사항,free1=자유게시판1,free2=자유게시판2...',
-`name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판 이름',
-delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부(0=삭제전, 1=삭제)',
-delDate DATETIME COMMENT '삭제날짜'
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	`code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice=공지사항,free1=자유게시판1,free2=자유게시판2...',
+	`name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판 이름',
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부(0=삭제전, 1=삭제)',
+	delDate DATETIME COMMENT '삭제날짜'
 );
 
 # 기본 게시판 생성
@@ -112,7 +113,7 @@ updateDate = NOW(),
 `code` = 'free1',
 `name` = '자유';
 
-# 게시판 테이블 boarId 칼럼 추가
+# 게시판 테이블에 boardId 칼럼 추가
 ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER memberId;
 
 # 1, 2번 게시물을 공지사항 게시물로 지정
@@ -125,4 +126,12 @@ UPDATE article
 SET boardId = 2
 WHERE id IN(3);
 
-
+#게시물 개수 늘리기(게시물 페이징을 위한)
+/*
+Insert into article
+(
+	regDate, updateDate, memberId, boardId, title, `body`
+)
+select now(), now(), floor(RAND() * 2)+1, FLOOR(RAND() * 2)+1, concat('제목_', RAND()), CONCAT('내용_', RAND())
+from article;
+*/
